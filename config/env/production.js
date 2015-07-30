1cb047d73b8f1f0cdb93ef2b63fa8d9c9a25aaf8
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2015-07-30 03:42:41 -0700

    Test

--- config/env/production.js
+++ config/env/production.js
@@ -1,11 +1,18 @@
 /*jslint node: true*/
 'use strict';
 
-var connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+var connection_string_default = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
+
+var connection_string = '';
+if (!process.env.MONGODB_URL) {
+  connection_string = connection_string_default;
+} else {
+  connection_string = process.env.MONGODB_URL + process.env.OPENSHIFT_APP_NAME;
+}
 
 module.exports = {
   db: connection_string,
