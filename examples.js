16cf0b57beecd4db7d0d4583ddf52883dd75a115
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2015-07-30 05:39:07 -0700

    Stabel version of api

--- examples.js
+++ examples.js
@@ -20,3 +20,34 @@ $.ajax({
   },
   dataType: "application/json"
 });
+
+
+
+// Serrialize to form to json data $('form').serializeObject()
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
+
+var $form = $("#" + 'addNewForm').parent();
+var formData = $form.serializeObject();
+$.ajax({
+  type: "POST",
+  url: "/api",
+  data: formData,
+  success: function() {
+    alert("Yey!");
+  },
+  dataType: "application/json"
+});
--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+  // add function to handle html form data to json object
+  $.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+      if (o[this.name] !== undefined) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || '');
+      } else {
+        o[this.name] = this.value || '';
+      }
+    });
+    return o;
+  };
 
   function setEventListenerToButton(buttonId) {
     $("#" + buttonId).click(function() {
@@ -8,7 +24,7 @@ $(document).ready(function() {
 
   function sendFormDataToServer(buttonId) {
     var $form = $("#" + buttonId).parent();
-    var formData = $form.serialize();
+    var formData = $form.serializeObject();
     if (formData) {
       $.ajax({
         type: "POST",
@@ -21,6 +37,8 @@ $(document).ready(function() {
       });
     }
   }
+
+  // TODO: getDataFromServer - get json object
 
   setEventListenerToButton("addNewForm");
 });
